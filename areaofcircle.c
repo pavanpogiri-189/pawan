1dfff3b3048dd226907a6d03ebf879742ce61172
pawan pogiri <pavanpogiri39@gmail.com>
2025-11-29 12:19:33 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    float r,sum;
+    float pie=3.14;
+    printf("Enter r value:");
+    scanf("%f",&r);
+    sum=pie*r*r;
+    printf("Area of circle is %f:",sum);
+}
---
+++ areaofrectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int l,b,sum;
+    printf("Enter l and b value:");
+    scanf("%d %d",&l,&b);
+    sum=l*b;
+    printf("Area of rectangle is: %d",sum);
+}
